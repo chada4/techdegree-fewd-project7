67cc5255bf921cc4ee29ddf1a860ca20bece9c3a
chada4 <87388946+chada4@users.noreply.github.com>
2021-08-30 19:19:59 -0400

    JS autocomplete and local storage
    - autocomplete function for search user in messaging works
    - save settings to local storage saves

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ const message = document.getElementById("messageField");
 const send = document.getElementById("send");
 const traffic = document.querySelector(".traffic-nav");
 
+
+
 alertBanner.innerHTML = 
 `
 <div class="alert-banner">
@@ -234,8 +236,103 @@ send.addEventListener('click', () => {
   }
 });
 
+// Autocomplete user search
 
+const userSearch = document.getElementById('userField');
+const userNames = ["Victoria Chambers", "Dale Byrd", "Dawn Wood", "Dan Oliver"];
 
+function autocomplete(inp, arr) {
+  let currentFocus;
+  inp.addEventListener("input", function(e) {
+      let a, b, i, val = this.value;
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      userField.insertAdjacentElement('afterend', a);
+      for (i = 0; i < arr.length; i++) {
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          b = document.createElement("DIV");
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+              b.addEventListener("click", function(e) {
+              inp.value = this.getElementsByTagName("input")[0].value;
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        currentFocus++;
+        addActive(x);
+      } else if (e.keyCode == 38) { 
+        currentFocus--;
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        e.preventDefault();
+        if (currentFocus > -1) {
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
+
+autocomplete(document.getElementById("userField"), userNames);
+
+// Local Storage
+
+const emailSetting = document.getElementById("setting-email");
+const profileSetting = document.getElementById("setting-profile");
+const timezone = document.getElementById("timezone");
+const save = document.getElementById("save");
+const cancel = document.getElementById("cancel");
+
+localStorage.getItem('emailSetting');
+localStorage.getItem('profileSetting');
+localStorage.getItem('timezone');
+
+save.onclick = saveSettings
+cancel.onclick = clearSettings
+
+function saveSettings() {
+  localStorage.setItem(emailSetting, emailSetting.value);
+  localStorage.setItem(profileSetting, profileSetting.value);
+  localStorage.setItem(timezone, timezone.value);
+}
+
+function clearSettings() {
+  localStorage.clear();
+}
 
 
 
